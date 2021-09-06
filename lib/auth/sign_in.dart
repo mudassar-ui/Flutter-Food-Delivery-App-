c4949d6b02a94211585a59824fe3fcaf5cddf046
mudassar-ui <mudassark15@yahoo.com>
2021-09-07 00:24:45 +0530

    Sign in Ui design code

---
+++ lib/auth/sign_in.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign'),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/background.png'),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 400,
+              width: double.infinity,
+              //color: Colors.red,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('sign in to continue'),
+                  Text(
+                    'Vegi',
+                    style:
+                        TextStyle(fontSize: 50, color: Colors.white, shadows: [
+                      BoxShadow(
+                        blurRadius: 5,
+                        color: Colors.green.shade900,
+                        offset: Offset(3, 3),
+                      ),
+                    ]),
+                  ),
+
+// with custom text
+                  Column(
+                    children: [
+                      SignInButton(
+                        Buttons.Apple,
+                        text: "Sign in with Apple",
+                        onPressed: () {},
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      SignInButton(
+                        Buttons.Google,
+                        text: "Sign in with Google",
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+
+                  Column(
+                    children: [
+                      Text(
+                        'By signing in you are agreeing to our',
+                        style: TextStyle(color: Colors.grey[800]),
+                      ),
+                      Text(
+                        'Terms and Privacy Policy',
+                        style: TextStyle(color: Colors.grey[800]),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
